36738c6f76fd49ba37a71a364fc12f61f4fdc40a
butkrit <59278110+butkrit@users.noreply.github.com>
2020-01-03 16:49:28 +0700

    Add files via upload

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+ double e =2.71828;	
+ int Y;
+ int X;
+ cout<<" Enter X ";
+ cin>> X;
 
+  cout<<" Y = "<<3*pow(X,3)+ 2*e + pow(2,2*X+1)+ sqrt((pow(X,2))+1);
+ return 0;
+
+}
--- lab4_2.cpp
+++ lab4_2.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
-//Write function findDistance() here
+double findDistance(double u,double a,double t){
+  double s = u*t+0.5*a*pow(t,2);
+  return s;
+}
 
 int main(){
 
-  //Calling findDistance() using test cases
+  double s = findDistance(5,4,3);
+  cout<< "s "<<s;
 
   return 0;
 }
